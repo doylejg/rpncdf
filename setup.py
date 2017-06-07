74ddb00647e0b3fcc3561d938dca27fd31334993
Jonathan Doyle <jonathan.g.doyle@gmail.com>
2017-06-07 16:47:35 -0400

    updated license

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ setup(name='rpncdf',
       description='Converts RPN standard files to NetCDF',
       classifiers=[
           'Development Status :: 4 - Beta',
-          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+
           'Programming Language :: Python :: 2.7+',
           'Intended Audience :: Science/Research',
           'Topic :: :: Linguistic',
@@ -14,7 +15,7 @@ setup(name='rpncdf',
       url='http://github.com/doylejg/rpncdf',
       author='Jonathan G. Doyle',
       author_email='jonathan.g.doyle@gmail.com',
-      license='GPLv3',
+      license='LGPLv3+',
       packages=['rpncdf'],
       install_requires=['numpy','netCDF4','rpnpy'],
       entry_points="""
